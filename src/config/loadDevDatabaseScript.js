73caedad41d52ccf8fc40ef21e065914be8ac035
Jonathan Alvarado <jalvarado@sold.com>
2024-08-26 17:35:12 -0400

    Add console.log for debug

--- src/config/loadDevDatabaseScript.js
+++ src/config/loadDevDatabaseScript.js
@@ -12,6 +12,25 @@ if (!dbUrl) {
   console.error('DB_URL is not defined in the .env file');
   process.exit(1);
 }
+function extractPostgresUriComponents(uri) {
+  try {
+    const url = new URL(uri);
+
+    const [username, password] = url.username
+      ? [url.username, url.password]
+      : [null, null];
+    const host = url.hostname;
+    const database = url.pathname ? url.pathname.split('/')[1] : null;
+
+    return { host, database, username, password };
+  } catch (error) {
+    console.error('Invalid URI:', error);
+    return null;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const components = extractPostgresUriComponents(dbUrl);
 
 // Read the database.config.json file
 const configPath = path.join(__dirname, 'database.config.json');
@@ -23,14 +42,21 @@ config.dev = {
   autoLoadModels: true,
   dialect: 'postgres',
   dialectOptions: {
-    ssl: true,
-    native: true,
+    ssl: {
+      required: true,
+      rejectUnauthorized: false,
+    },
   },
   uri: dbUrl,
   synchronize: true,
+  ...components,
 };
 
+const json = JSON.stringify(config, null, 2);
+
+console.log('Resulting config:', json);
+
 // Save the database.config.json file with the new entry
-fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+fs.writeFileSync(configPath, json);
 
 console.log('Successfully added "dev" entry to database.config.json');
